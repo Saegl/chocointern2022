1e144496d30c010c94f33bc5e5d5d70ebc5d75a6
Saegl <saegl@protonmail.com>
2022-02-19 20:39:21 +0600

    Using redis.pipeline for offers save

--- mini_showcase/tasks.py
+++ mini_showcase/tasks.py
@@ -7,9 +7,13 @@ from mini_showcase import providers
 async def load_search_and_save(redis: Redis, search_id, request_data):
     provider_response = await providers.offers_search(request_data)
 
-    # Save provider_response for "get_search_by_id" handler
+    # Save provider_response for "app.get_search_by_id" handler
     await redis.set(search_id, ujson.dumps(provider_response))
 
-    # Save offers for "get_offer_by_id" handler
-    for offer in provider_response["items"]:
-        await redis.set(f'offer:{offer["id"]}', ujson.dumps(offer))
+    # Save offers for "app.get_offer_by_id" handler
+    async with redis.pipeline(transaction=True) as pipe:
+        for offer in provider_response["items"]:
+            pipe.set(f'offer:{offer["id"]}', ujson.dumps(offer))
+
+        commands_success = await pipe.execute()
+        assert all(commands_success)
